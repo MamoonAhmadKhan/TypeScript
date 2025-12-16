c9ab9656b1784fadacee3f5a4b071f5eb9f887da
MamoonAhmadKhan <khanmamoon887@gmail.com>
2025-12-16 21:58:06 +0530

    learnt more about Interfaces

--- src/05 interface.ts
+++ src/05 interface.ts
@@ -1,30 +1,82 @@
-interface Person {
+// * * * INTERFACES * * *
+
+// Interfaces & Types can be used interchangeabley, but for objects - Interfaces are preferrable
+
+// Interface with objects
+interface User {
     name: string;
+    email: string;
+    password: string | number;
     age: number;
-    caste?: string;
-    greet (phrase: string): void;
 }
 
-class Employee implements Person {
-    name: string;
-    age: number;
-    caste?: string;
+const user1: User = {
+    name: "Mamoon",
+    email: "mamoon12@gmail.com",
+    password: "Mamoon1234",
+    age: 22,
+}
 
-    constructor (name: string, age: number, caste: string = "Unkown!") {
-        this.name = name;
-        this.age = age;
-        this.caste = caste;
-    }
+// Interface with functions
+interface UserData {
+    (user: User): Partial <User>;
+}
 
-    greet = (phrase: string): void => {
-        console.log(`${phrase}, ${this.name}`);
-        console.log(`Your age is ${this.age}`);
-        console.log(`You are ${this.caste} by caste`);
+const getUserData: UserData = (user: User): Partial <User> => {
+    return {
+        name: user.name,
+        age: user.age,
+        email: user.email,
     }
 }
 
-const Engineer = new Employee("Mamoon", 22, "Pakhtoon");
-Engineer.greet("Good Morning");
+const mamoon = getUserData(user1);
+console.log(mamoon);
 
-const Analyst = new Employee("Pratham", 23, "Unkown");
-Analyst.greet("Oye Bihari");
+const adeel = getUserData({
+    name: "Adeel",
+    email: "adeel87@gmail.com",
+    password: 8734234,
+    age: 24,
+});
+console.log(adeel);
+
+
+
+
+
+
+
+
+// * * * INTERFACE IMPLEMENTATION IN CLASSES * * *
+
+// interface Person {
+//     name: string;
+//     age: number;
+//     caste?: string;
+//     greet (phrase: string): void;
+// }
+
+// class Employee implements Person {
+//     name: string;
+//     age: number;
+//     caste?: string;
+
+//     constructor (name: string, age: number, caste: string = "Unkown!") {
+//         this.name = name;
+//         this.age = age;
+//         this.caste = caste;
+//     }
+
+//     greet = (phrase: string): void => {
+//         console.log(`${phrase}, ${this.name}`);
+//         console.log(`Your age is ${this.age}`);
+//         console.log(`You are ${this.caste} by caste`);
+//     }
+// }
+
+// const Engineer = new Employee("Mamoon", 22, "Pakhtoon");
+// Engineer.greet("Good Morning");
+
+// const Analyst = new Employee("Pratham", 23, "Unkown");
+// Analyst.greet("Oye Bihari");
